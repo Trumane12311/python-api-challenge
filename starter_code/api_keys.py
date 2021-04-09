7c7bb867e13e1c5bc71b9c51883cb7473d09d445
Trumane12311 <Trumane12311@gmail.com>
2021-04-08 21:12:40 -0400

    First section code

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "91ab7f951f6b967abfd3ef98032c2b43"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAhZqEv9EUrmrUGhPJYJSliTbKJe1kzTKo"
